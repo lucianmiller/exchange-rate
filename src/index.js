3f669ed0629c917d6fe31592cbf441778a5b9eb3
Lucian Miller <lucian.miller185@gmail.com>
2022-04-18 09:33:36 -0700

    Add promise for exchange rate API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import $ from 'jquery';
 import './css/styles.css';
+import Rates from'./rates.js'
 
 $(document).ready(function() {
   $("#exchange").submit(function(event) {
-    const usdAmount = parseInt($("#usd").val());
-    const userCurrency = $("#conversion-options").val();
-    console.log(usdAmount);
-    console.log(userCurrency);
+    let promise = Rates.getRates();
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      const usdAmount = parseInt($("#usd").val());
+      const userCurrency = $("#conversion-options").val();
+      console.log(body);
+      console.log(usdAmount);
+      console.log(userCurrency);
+    }, function(error) {
+      $('#error').text(`There was an error processing your request: ${error}`)
+    });
     event.preventDefault();
   });
 });
